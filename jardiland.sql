d2041b7b008a12bada222cfc83d6d970376f0c16
Antoine PECQUET <antoine@weecop.fr>
2016-12-15 18:54:24 +0100

    SQL creation

---
+++ jardiland.sql
@@ -0,0 +1,304 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Jeu 15 Décembre 2016 à 09:52
+-- Version du serveur :  5.7.16
+-- Version de PHP :  7.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `jardiland`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `catalog`
+--
+
+CREATE TABLE `catalog` (
+  `idProvider` int(11) NOT NULL,
+  `idProduct` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Plante'),
+(2, 'Fleur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `forname` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `reduction` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `customer`
+--
+
+INSERT INTO `customer` (`id`, `name`, `forname`, `email`, `address`, `password`, `reduction`) VALUES
+(1, 'Nium', 'Gérard', 'gerard.nium@weecop.fr', '6 rue de la prairie', 'password', NULL),
+(2, 'a', 'a', 'a', 'a', 'a', NULL),
+(3, 'b', 'b', 'b', 'b', 'b', '50');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `drive`
+--
+
+CREATE TABLE `drive` (
+  `idOrders` int(11) NOT NULL,
+  `idProduct` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `total` decimal(10,0) NOT NULL,
+  `idCustomer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `species` varchar(256) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `idCategory` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `species`, `price`, `idCategory`) VALUES
+(1, 'Anthémis', 'Asteraceae', '11', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `provide`
+--
+
+CREATE TABLE `provide` (
+  `idProduct` int(11) NOT NULL,
+  `idProvider` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `provider`
+--
+
+CREATE TABLE `provider` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `forname` varchar(256) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `city` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `supply`
+--
+
+CREATE TABLE `supply` (
+  `id` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `idProvider` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `catalog`
+--
+ALTER TABLE `catalog`
+  ADD KEY `idProvider` (`idProvider`,`idProduct`),
+  ADD KEY `idProduct` (`idProduct`);
+
+--
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `drive`
+--
+ALTER TABLE `drive`
+  ADD KEY `idOrders` (`idOrders`,`idProduct`),
+  ADD KEY `idProduct` (`idProduct`);
+
+--
+-- Index pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCustomer` (`idCustomer`);
+
+--
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCategory` (`idCategory`);
+
+--
+-- Index pour la table `provide`
+--
+ALTER TABLE `provide`
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idProvider` (`idProvider`);
+
+--
+-- Index pour la table `provider`
+--
+ALTER TABLE `provider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `supply`
+--
+ALTER TABLE `supply`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idProvider` (`idProvider`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `provider`
+--
+ALTER TABLE `provider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `supply`
+--
+ALTER TABLE `supply`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `catalog`
+--
+ALTER TABLE `catalog`
+  ADD CONSTRAINT `catalog_ibfk_1` FOREIGN KEY (`idProvider`) REFERENCES `provider` (`id`),
+  ADD CONSTRAINT `catalog_ibfk_2` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`);
+
+--
+-- Contraintes pour la table `drive`
+--
+ALTER TABLE `drive`
+  ADD CONSTRAINT `drive_ibfk_1` FOREIGN KEY (`idOrders`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `drive_ibfk_2` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`);
+
+--
+-- Contraintes pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`idCustomer`) REFERENCES `customer` (`id`);
+
+--
+-- Contraintes pour la table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`idCategory`) REFERENCES `category` (`id`);
+
+--
+-- Contraintes pour la table `provide`
+--
+ALTER TABLE `provide`
+  ADD CONSTRAINT `provide_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `provide_ibfk_2` FOREIGN KEY (`idProvider`) REFERENCES `provider` (`id`);
+
+--
+-- Contraintes pour la table `supply`
+--
+ALTER TABLE `supply`
+  ADD CONSTRAINT `supply_ibfk_1` FOREIGN KEY (`idProvider`) REFERENCES `provider` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
